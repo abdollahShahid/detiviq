17607094892695441f0d6209e4f7f8ac1841afb3
Zuhair <zuhair@dcpracticegrowth.com>
2026-04-05 06:47:51 +0500

    Prepare backend for deployment

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -13,7 +13,7 @@ from app.core.config import settings
 from app.db.base import Base
 
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL.replace("%", "%%"))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
